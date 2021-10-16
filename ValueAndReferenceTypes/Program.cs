d8ac6a3790c7ef45a699c64b14ffa15081eb778c
erayberatatalay <erayberat37@gmail.com>
2021-10-16 08:45:03 +0300

    .

--- ValueAndReferenceTypes/Program.cs
+++ ValueAndReferenceTypes/Program.cs
@@ -10,7 +10,15 @@ namespace ValueAndReferenceTypes
 
             int number1 = 10;
             int number2 = 20;
+            number1 = number2;
+            number2 = 50;
+            Console.WriteLine("Değer Tipi => " + number1); //20
 
+            int[] numbers1 = new int[] { 10, 20, 30};
+            int[] numbers2 = new int[] { 100, 200, 300 };
+            numbers1 = numbers2;
+            numbers2[0] = 999;
+            Console.WriteLine("Referans Tipi => " + numbers2[0]); //999
         }
     }
 }
