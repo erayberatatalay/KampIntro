92bf9b14ef16dcdaa9016e89cf4d72a01023b73c
erayberatatalay <erayberat37@gmail.com>
2021-10-16 17:41:46 +0300

    .

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,17 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsStock = 3;
+
+            Product product2 = new Product {Id=2, CategoryId=5, UnitsStock=5, ProductName="Kalem",UnitPrice=35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
         }
     }
 }
